eebee5dda9a3482d056cea9199c5e81363ace3ae
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-07-26 17:02:34 -0400

    update script, file structure

--- create-redirects.py
+++ create-redirects.py
@@ -1,41 +1,44 @@
+SOURCE_FILENAME = "spark-connector.txt"
+SOURCE_FILE = './mut-redirects/'+SOURCE_FILENAME
+DESTINATION_FILE = "./netlify-redirects/netlify-"+SOURCE_FILENAME
+BASE = "docs/spark-connector"
 
 
 def transform(rule: str) -> str:
-    print(rule)
+    # remove base if in rule
     if rule.startswith(" ${base}"):
         rule = rule.split(" ${base}")[1]
+    # remove version if in path
     if rule.startswith("/${version}"):
-        print("START")
         rule = rule.split("/${version}")[1]
     ##add leading slash to rules
-    print(rule)
     return rule
 
 def main() -> None:
-    source_file =  "docs-k8s-operator.txt"
-    destination_file = "netlify-docs-k8s-operator.txt"
-    from_base = "docs/kubernetes-operator"
-
-    with open(source_file, "r") as f:
+    with open(SOURCE_FILE, "r") as f:
         rules = [
             (rule[0].split(None, 1)[1], rule[1])
+            ##split each line into origin and destination path
             for rule in (line.split("->", 1) for line in f.read().split("\n"))
+            ##check that the line parsed is a redirect rule and not a symlink rule 
             if len(rule) >1
+            if not rule[0].startswith("symlink")
         ]
     output_rules = []
 
     for rule_from, rule_to in rules[2:]:
-        rule_from = rule_from.split(from_base)[1]
+        if not rule_from.find(BASE) == -1:
+            rule_from = rule_from.split(BASE)[1]
+        elif not rule_from.find("${prefix}") == -1:
+            rule_from = rule_from.split("${prefix}")[1]
         rule_from = transform(rule_from)
         rule_to = transform(rule_to)
 
         
-        ##replace version, delete base and or/stable in rule to
         output_rules.append("from = "+ rule_from + "\rto = "+ rule_to + "\r")
-        # print(f"raw: {rule_from} -> {rule_to}")
 
-    with open(destination_file, "w") as f:
-        f.write("[[redirects]] \r" + (" \r [[redirects]] \r").join(output_rules))
+    with open(DESTINATION_FILE, "w") as f:
+        f.write("[[redirects]] \r" + (" \r[[redirects]] \r").join(output_rules))
 
 
 
